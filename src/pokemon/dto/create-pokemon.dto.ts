471a7eeb2859978bbb802e7663b8bb5518321133
Juan Sebastian Artunduaga Rojas <jartunduaga7896@gmail.com>
2022-09-10 04:11:03 -0500

    Fin sección 7

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,15 @@
+import { IsInt, IsPositive, IsString, Min, MinLength } from "class-validator";
+
+export class CreatePokemonDto {
+
+    // isInt, isPositive, minLength 1
+    @IsInt()
+    @IsPositive()
+    @Min(1)
+    no: number;
+
+    // isString, minLength 1
+    @IsString()
+    @MinLength(1)
+    name: string;
+}
